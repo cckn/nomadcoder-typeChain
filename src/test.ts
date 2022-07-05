e4c1424b3577d22ea61c09926df10ba50493f8c2
cckn <cckn.dev@gmail.com>
2022-07-06 06:45:54 +0900

    .

--- src/test.ts
+++ src/test.ts
@@ -37,107 +37,52 @@ class LocalStorageAPI<T> extends LocalStorageAbstractClass<T> {
  * GeolocationAPI
  *---------------------------------------------------------------------------*/
 
-interface GeolocationPosition {
-	coords: {
-		latitude: number
-		longitude: number
-		altitude: number
-		accuracy: number
-		altitudeAccuracy: number
-		heading: number
-		speed: number
-	}
-	timestamp: number
-}
-
-interface GeolocationPositionError {
-	code: number
-	message: string
-}
-
-interface GeolocationPositionOptions {
+type SuccessFn = (position: GeolocationPosition) => void
+type ErrorFn = (error: GeolocationPositionError) => void
+interface Options {
 	enableHighAccuracy: boolean
 	timeout: number
 	maximumAge: number
 }
 
-type SuccessFn = (position: GeolocationPosition) => void
-type ErrorFn = (error: GeolocationPositionError) => void
-type WatchId = number
-
-interface GeolocationAPI {
+interface GeolocationInterface {
 	getCurrentPosition(successFn: SuccessFn): void
 	getCurrentPosition(successFn: SuccessFn, errorFn: ErrorFn): void
-	getCurrentPosition(successFn: SuccessFn, errorFn: ErrorFn, options: GeolocationPositionOptions): void
-
-	watchPosition(successFn: SuccessFn): WatchId
-	watchPosition(successFn: SuccessFn, errorFn: ErrorFn): WatchId
-	watchPosition(successFn: SuccessFn, errorFn: ErrorFn, options: GeolocationPositionOptions): WatchId
-
-	clearWatch(watchId: WatchId): void
+	getCurrentPosition(successFn: SuccessFn, errorFn: ErrorFn, options: Options): void
+	watchPosition(successFn: SuccessFn): number
+	watchPosition(successFn: SuccessFn, errorFn: ErrorFn): number
+	watchPosition(successFn: SuccessFn, errorFn: ErrorFn, options: Options): number
+	clearWatch(watchId: number): void
 }
 
-class GeolocationAPI implements GeolocationAPI {
-	private geolocationPosition: GeolocationPosition = {
-		coords: {
-			latitude: 0,
-			longitude: 0,
-			altitude: 0,
-			accuracy: 0,
-			altitudeAccuracy: 0,
-			heading: 0,
-			speed: 0,
-		},
-		timestamp: 0,
+class GeolocationAPI implements GeolocationInterface {
+	geolocation: Geolocation
+
+	constructor() {
+		this.geolocation = new Geolocation()
 	}
-
-	private geolocationPositionError: GeolocationPositionError = {
-		code: 0,
-		message: "",
-	}
-
-	private watchIds: Record<WatchId, { successFn: SuccessFn; errorFn: ErrorFn; options: GeolocationPositionOptions }> =
-		{}
-
-	private prevWatchId = 0
 
 	getCurrentPosition(successFn: SuccessFn): void
 	getCurrentPosition(successFn: SuccessFn, errorFn: ErrorFn): void
-	getCurrentPosition(successFn: SuccessFn, errorFn: ErrorFn, options: GeolocationPositionOptions): void
-	getCurrentPosition(successFn: SuccessFn, errorFn?: ErrorFn, options?: GeolocationPositionOptions): void {
-		successFn(this.geolocationPosition)
-
-		if (errorFn) {
-			errorFn(this.geolocationPositionError)
-		}
-
-		if (options) {
-			if (options.enableHighAccuracy) {
-				console.log("enableHighAccuracy")
-			}
-			if (options.timeout) {
-				console.log("timeout")
-			}
-			if (options.maximumAge) {
-				console.log("maximumAge")
-			}
-		}
+	getCurrentPosition(successFn: SuccessFn, errorFn: ErrorFn, options: Options): void
+	getCurrentPosition(successFn: SuccessFn, errorFn?: ErrorFn, options?: Options): void {
+		this.geolocation.getCurrentPosition(successFn, errorFn, options)
 	}
 
-	watchPosition(successFn: SuccessFn): WatchId
-	watchPosition(successFn: SuccessFn, errorFn: ErrorFn): WatchId
-	watchPosition(successFn: SuccessFn, errorFn: ErrorFn, options: GeolocationPositionOptions): WatchId
-	watchPosition(successFn: SuccessFn, errorFn?: ErrorFn, options?: GeolocationPositionOptions): WatchId {
-		const watchId = this.prevWatchId++
-		this.watchIds[watchId] = {
-			successFn,
-			errorFn,
-			options,
-		}
+	watchPosition(successFn: SuccessFn): number
+	watchPosition(successFn: SuccessFn, errorFn: ErrorFn): number
+	watchPosition(successFn: SuccessFn, errorFn: ErrorFn, options: Options): number
+	watchPosition(successFn: SuccessFn, errorFn?: ErrorFn, options?: Options): number {
+		const watchId = this.geolocation.watchPosition(successFn, errorFn, options)
 		return watchId
 	}
 
-	clearWatch(watchId: WatchId): void {
-		delete this.watchIds[watchId]
+	clearWatch(watchId: number): void {
+		this.clearWatch(watchId)
 	}
 }
+
+const geolocation = new GeolocationAPI()
+geolocation.getCurrentPosition((position: GeolocationPosition) => {
+	console.log(position)
+})
